4522519fc5bb7067b45dc301fa423e69dc6a7b82
Matt Cleary <mattcleary@Matts-MBP.lan>
2018-02-04 16:51:34 +0000

    wired up loadData functions for ssr

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,22 @@
-const express = require('express')
+import 'babel-polyfill'
+import express from 'express'
+import React from 'react'
+import { matchRoutes } from 'react-router-config'
+import Routes from './client/Routes'
+import renderer from './helpers/renderer'
+import createStore from './helpers/createStore'
+
 const app = express()
 
-app.get('/', (req, res) => {})
+app.use(express.static('public'))
+app.get('*', async (req, res) => {
+  const store = createStore()
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null
+  })
+  await Promise.all(promises)
+  res.send(renderer(req, store))
+})
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
